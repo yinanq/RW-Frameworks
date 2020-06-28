b1df963f58f88bc2c4466467e23295b6682943e6
Yinan <yinancode@gmail.com>
2020-06-27 21:02:24 -0700

    added a Networking API

--- Sources/RW-Frameworks/Networking.swift
+++ Sources/RW-Frameworks/Networking.swift
@@ -9,6 +9,23 @@ import Foundation
 
 extension RW_Frameworks {
     public class Networking {
+        public class Manager {
+            public init() {}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { data, response, error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+        }
         
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
 }
--- Tests/RW-FrameworksTests/RwfNetworkingTests.swift
+++ Tests/RW-FrameworksTests/RwfNetworkingTests.swift
@@ -6,16 +6,30 @@
 //
 
 import XCTest
+@testable import RW_Frameworks
 
 final class RwfNetworkingTests: XCTestCase {
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testLoadDataCall() {
+        let manager = RW_Frameworks.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard let url = URL(string: "https://raywenderlich.com") else {
+            return XCTFail("Could not create URL")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
     }
     
     static var allTests = [
-        ("testExample", testExample)
+        ("testLoadDataCall", testLoadDataCall)
     ]
 
 }
